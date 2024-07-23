a1bcb260b330d88ab776b8e01f85489918adc070
Sergii Illichevskyi <sergii.illichevskyi@graviteesource.com>
2024-07-23 16:43:18 +0200

    feat(console): Portal admin can configure custom portal banner in console UI

--- gravitee-apim-console-webui/src/portal/customization/banner/portal-banner.component.ts
+++ gravitee-apim-console-webui/src/portal/customization/banner/portal-banner.component.ts
@@ -14,30 +14,96 @@
  * limitations under the License.
  */
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
-import { ReactiveFormsModule, Validators, FormControl, FormGroup } from '@angular/forms';
+import { ReactiveFormsModule, Validators, FormControl, FormGroup, FormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import { GioSaveBarModule } from '@gravitee/ui-particles-angular';
+import {GioFormSlideToggleModule, GioSaveBarModule} from '@gravitee/ui-particles-angular';
+import { MatRadioButton, MatRadioGroup } from '@angular/material/radio';
+import { of } from 'rxjs';
+
+import { BannerType, BannerEnum } from '../../../entities/management-api-v2/documentation/bannerType';
+import { GioRoleModule } from '../../../shared/components/gio-role/gio-role.module';
+import {MatOption} from "@angular/material/autocomplete";
+import {MatSelect} from "@angular/material/select";
+import {Group} from "../../../entities/management-api-v2";
+import {MatSlideToggle} from "@angular/material/slide-toggle";
+
+interface BannerForm {
+  bannerType: FormControl<BannerType>;
+  titleText: FormControl<string>;
+  subTitleText: FormControl<string>;
+  primaryButtonText: FormControl<string>;
+  primaryButtonEnabled: FormControl<boolean>;
+  primaryButtonRedirection: FormControl<string[]>;
+  secondaryButtonText: FormControl<string>;
+  secondaryButtonEnabled: FormControl<boolean>;
+  secondaryButtonRedirection: FormControl<string[]>;
+}
+
+export interface PageRedirection {
+  id?: string;
+  name?: string;
+}
 
 @Component({
   selector: 'portal-banner',
   templateUrl: './portal-banner.component.html',
   styleUrls: ['./portal-banner.component.scss'],
-  imports: [CommonModule, GioSaveBarModule, MatCardModule, MatFormFieldModule, MatInputModule, ReactiveFormsModule],
+  imports: [
+    CommonModule,
+    GioSaveBarModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    MatRadioButton,
+    MatRadioGroup,
+    GioRoleModule,
+    FormsModule,
+    MatOption,
+    MatSelect,
+    GioFormSlideToggleModule,
+    MatSlideToggle,
+  ],
   standalone: true,
 })
-export class PortalBannerComponent {
-  public developerPortalBannerForm = new FormGroup({
-    name: new FormControl(null, Validators.required),
-  });
+export class PortalBannerComponent implements OnInit {
+  form: FormGroup<BannerForm>;
+  bannerTypes = Object.values(BannerEnum);
+  pageRedirections: PageRedirection[] = [
+    {name: 'Top Bar', id: '0'},
+    {name: 'Catalog', id: '1'},
+    {name: 'API', id: '2'},
+    {name: 'Banner', id: '3'},
+    {name: 'Theme', id: '4'},
+  ];
+
+  ngOnInit(): void {
+    console.log('pageRedirections size: ' + this.pageRedirections.length);
+    this.form = new FormGroup<BannerForm>({
+      bannerType: new FormControl<BannerType>(BannerEnum.NONE, [Validators.required]),
+      titleText: new FormControl<string>('', [Validators.required]),
+      subTitleText: new FormControl<string>('', [Validators.required]),
+      primaryButtonText: new FormControl<string>('', [Validators.required]),
+      primaryButtonRedirection: new FormControl<string[]>([]),
+      primaryButtonEnabled: new FormControl<boolean>(false),
+      secondaryButtonText: new FormControl<string>('Explore APIs', [Validators.required]),
+      secondaryButtonEnabled: new FormControl<boolean>(false),
+      secondaryButtonRedirection: new FormControl<string[]>([]),
+    });
+  }
 
   reset() {
-    this.developerPortalBannerForm.reset();
+    this.form.reset({
+      bannerType: BannerEnum.NONE,
+    });
   }
 
   submit() {
     // console.log("Form submitted 🚀");
   }
+
+  protected readonly of = of;
 }
